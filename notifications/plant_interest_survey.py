76b134819cc69cb860118e29519735026258383f
Solomiia <skachur@olin.edu>
2023-05-05 07:38:14 -0400

    Added pytest

--- notifications/plant_interest_survey.py
+++ notifications/plant_interest_survey.py
@@ -9,7 +9,15 @@ class PlantInterest:
 
     def get_plants_interest(self):
         """
-        Docstring
+        Asks the user which plant they are interested in and sets the `plants_interest` attribute to the selected plant.
+
+        This method displays a list of suggested plants and prompts the user to select one. The user's choice is validated
+        to ensure it is a number between 1 and the number of suggested plants. If the input is valid, the `plants_interest`
+        attribute is set to the selected plant and the method
+
+        Returns :
+            A string of the selected plant. If the input is invalid, an error
+            message is displayed and the method returns `None`.
         """
         suggested_plants = ["Cucumber", "Kale", "Tomato", "Basil"]
         print("Which plant are you interested in?")
@@ -24,7 +32,15 @@ class PlantInterest:
 
     def get_watering_time(self):
         """
-        Docstring
+        Prompts the user to enter the time of day when it is best to water their plants.
+
+        This method asks the user to enter a number between 0 and 23 to represent the time of day when it is best to water
+        their plants. The input is validated to ensure it is a valid integer within the range. If the input is invalid,
+        an error message is displayed and the user is prompted again. Once a valid input is provided, the method returns the
+        integer value.
+
+        Return:
+            An integer value representing the time of day when it is best to water the plants.
         """
         time = input(
             "What time of day (0-23) is best for watering your plants? "
@@ -36,7 +52,14 @@ class PlantInterest:
 
     def get_user_input(self):
         """
-        Docstring
+        Asks the user for input on the plants they are interested in and the best time to water them.
+
+        This method calls the `get_plants_interest` and `get_watering_time` methods to prompt the user for input on the plants
+        they are interested in and the best time of day to water them. The `plants_interest` and `watering_time` attributes
+        of the instance are set to the values returned by the respective methods.
+
+        Return:
+            None
         """
         self.get_plants_interest()
         self.get_watering_time()
